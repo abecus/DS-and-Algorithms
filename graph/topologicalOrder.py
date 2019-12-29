ec8a72b4e4c580c5da1a390d191ffe4edb209e5d
abecus <insaaone@gmail.com>
2019-12-29 16:38:45 +0530

    graph algorithms: connected components

--- graph/topologicalOrder.py
+++ graph/topologicalOrder.py
@@ -3,19 +3,16 @@ from collections import OrderedDict
 
 def topologicalSort(graph):
     visited = OrderedDict()
-        
+
     def dfs(node):
-        if visited.get(node, False):
-            return
-        # i=1
-        for adjnode in graph[node].keys():
-            dfs(adjnode)
-            visited[adjnode] = 1
-            # i+=1
-            
+        if not visited.get(i, 0):
+            for adjnode in graph[node]:
+                dfs(adjnode)
+                visited[adjnode] = 1
+
     for i in graph.keys():
-        dfs(i)     
-    
+        dfs(i)
+
     visited.update({key:1 for key in graph.keys()})
     return list(visited.keys())
 
