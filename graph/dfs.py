bd5c64d90346a9fefbad06b412d2ee8c75e45740
abecus <insaaone@gmail.com>
2020-01-21 20:40:06 +0530

    updated previous method

--- graph/dfs.py
+++ graph/dfs.py
@@ -13,41 +13,38 @@ def dfs(G, start, end):
 	"""
 	if start==end:
 		return end
-
-	visited = set()
-	path = []
  
-	def helper(node):
+	def helper(node, path):
 		if node == end:
 			# found the end node
 			return node
 
 		visited.add(node)
 		# add node to visited
-  
-		if node in G.graph:
-			for child in G.graph[node]:
-				# looping through all node which are 
-				# connected by edges
 
-				if child not in visited:
-					# if nodes are not visited before explore
+		for child, _ in G.getAdjcentNodes(node):
+			# looping through all node which are 
+			# connected by edges
 
-					if helper(child)!=None:
-						path.append(child)
-						return path
+			if child not in visited:
+				# if nodes are not visited before explore
+
+				if helper(child, path)!=None:
+					path.append(child)
+					return path
+
 	
+	visited = set()
+	path = []
 	if start in G.graph:
-		path = helper(start)
-		visited.add(start)
-		path.append(start)
+		path = helper(start, path)
 		
 		if path != None:
 			# if path has been found return it
-			return [*reversed(path)]
+			return [*reversed(path+[start])]
 
 	return f"Path does not exists between the Nodes ({start} to {end})"
 
 if __name__ == "__main__":
     
-	print(dfs(g5, "s", "g"))
+	print(dfs(g6, "a", "m"))
