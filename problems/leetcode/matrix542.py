aa316ad942a65f15c2e7a43a1e1da94436044f35
abecus <insaaone@gmail.com>
2020-07-31 00:11:51 +0530

    leetcode 542

--- problems/leetcode/matrix542.py
+++ problems/leetcode/matrix542.py
@@ -32,42 +32,47 @@ The cells are adjacent in only four directions: up, down, left and right.
 """
 
 from collections import deque
+from typing import *
+import math
 
-def updateMatrix(mat):
-    row = len(mat)
-    col = len(mat[0])
-    q = deque()
-    
-    for i in range(row):
-        for j in range(col):
-            val = mat[i][j]
-            if not val:
-                q.append((i ,j, val))
-                
-    
-    visited = set()
-    while q:
-        x, y, d = q.popleft()            
-        for dx, dy in zip([1,0,-1,0], [0,1,0,-1]):
-            nx = x+dx
-            ny = y+dy
-            if (nx,ny) not in visited and\
-                0<=nx<row and 0<=ny<col and mat[nx][ny]:
-                    mat[nx][ny] = d+1
-                    q.append((nx,ny,d+1))
-                    visited.add((nx,ny))
-                                   
-    return mat
 
-        
+class Solution:
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        row = len(matrix)
+        col = len(matrix[0])
+        q = deque()
+
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == 0:
+                    q.append((i, j))
+                else:
+                    matrix[i][j] = math.inf
+
+        while q:
+            x, y = q.popleft()
+            for dx, dy in zip([1, 0, -1, 0], [0, 1, 0, -1]):
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < row and 0 <= ny < col and matrix[nx][ny] > matrix[x][y]+1:
+                    matrix[nx][ny] = matrix[x][y]+1
+                    q.append((nx, ny))
+
+        return matrix
+
+
 if __name__ == "__main__":
-    mat = [[0,0,0], [0,1,0], [1,1,1]]
-    mat = [[0,1,1], 
-           [1,1,1], 
-           [1,1,1]]
-    # mat = [[1,1,1], 
-    #        [1,1,1], 
+    mat = [[0, 0, 0],
+           [0, 1, 0],
+           [1, 1, 1]]
+
+    mat = [[0, 1, 1],
+           [1, 1, 1],
+           [1, 1, 1]]
+
+    # mat = [[1,1,1],
+    #        [1,1,1],
     #        [1,1,0]]
+
     ans = updateMatrix(mat)
-    for i in ans:
-        print(i)
+    print(*ans, sep='\n')
