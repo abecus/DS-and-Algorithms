33cff3368c58b6b7c89d2651a02ab1628ea2aaf3
abecus <insaaone@gmail.com>
2020-08-04 16:33:51 +0530

    mandelbrot set update for faster computatioin

--- math/geomertry/mendelbrotSet.py
+++ math/geomertry/mendelbrotSet.py
@@ -1,3 +1,4 @@
+from alive_progress import alive_bar
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,43 +9,46 @@ def get_iter(c:complex, thresh:int =4, max_steps:int =25) -> int:
     z=c
     i=1
     while i<max_steps and (z*z.conjugate()).real < thresh:
-        z=z*z +c
+        z = z*z + c
         i+=1
     return i
 
 def plotter(n, thresh, max_steps=25):
-
-    # image_coordinate (integers) --> real value to use on 
-    # complex plane
-    mapper = lambda x,y: (4*(x-n//2)/n, 4*(y-n//2)/n)
-
-    # create image array (n*n) fill value 255
-    img=np.full((n,n), 255)
-    
     # since we know the bounds of x and y use it to save 
     # redundant computations
     # x \in {-2, 0.47} , y \in {-1.17, 1.17}
-    x_lower = 0
-    x_upper = 5*n//8
-    y_lower = 2*n//10
-    y_upper = 8*n//10
     
-    for x in range(x_lower, x_upper):
-        for y in range(y_lower, y_upper):
-            
-            it = get_iter(complex(*mapper(x,y)), 
-                           thresh=thresh, 
-                           max_steps=max_steps)
-            
-            # image get rotated with -90 dergee (don't know why)
-            # so know rotating 90 by changing coordinate below
-            img[y][x] = 255 - it
+    # image_coordinate (integers) --> real value to use 
+    # on complex plane
+    mx = 2.48 / (n-1)
+    my = 2.26 / (n-1)
+    mapper = lambda x,y: (mx*x - 2, my*y - 1.13)
 
-    return img[y_lower:y_upper, x_lower:x_upper]
+    # create image array (n*n) fill value 255
+    img = np.full((n,n), 255)
+    
+    with alive_bar(n*n) as bar:
+        for x in range(n):
+            for y in range(n):
+                
+                it = get_iter(complex(*mapper(x,y)), 
+                                thresh=thresh, 
+                                max_steps=max_steps)
+                
+                # image get rotated with -90 dergee (don't know why)
+                # so know rotating 90 by changing coordinate below
+                img[y][x] = 255 - it
+                bar()
+
+        return img
 
 
-n=1000
-img = plotter(n, thresh=4, max_steps=50)
-plt.imshow(img, cmap="plasma")
-plt.axis("off")
-plt.show()
+if __name__ == "__main__":
+    n=12500
+    img = plotter(n, thresh=4, max_steps=50)
+    np.save("mandelbrotSetNP_12500", img)
+    # img=np.load("mandelbrotSetNP_1000.npy")
+    plt.imshow(img, cmap="plasma")
+    plt.axis("off")
+    plt.show()
+
