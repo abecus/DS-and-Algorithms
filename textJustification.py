dc95250c1ef531276668cea7a42da70de0bddcae
abecus <insaaone@gmail.com>
2020-08-16 16:09:41 +0530

    added recursive solution too

--- textJustification.py
+++ textJustification.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from itertools import accumulate, islice
+import functools, math
 
 
 m = float("inf")
@@ -46,6 +47,69 @@ def textJustification(text, lineWidth=80):
 		i=breakPoints[i]
 
 
+import itertools
+def textJustification2(words, l):
+	# converting words into array of words
+	words = words.split(' ')
+	parent = {}
+	accumulated_Length = [0]+[*itertools.accumulate(map(len, words))]
+
+	def getLength(i, j):
+		# returns the length of word from including spacing between them
+		return accumulated_Length[j+1] - accumulated_Length[i] + j - i
+
+	def computeCost(lineWidth, wordsLength):
+		# coputes the cost functions which has to be minimized
+		return pow(lineWidth - wordsLength, 3)
+
+
+	@functools.lru_cache(None)
+	def foo(i):
+		if i == len(words):
+			# no word remains
+			return 0
+
+		cost = math.inf
+		for j in range(i, len(words)):
+			wordLength = getLength(i, j)
+			
+			# check if words can fit in the a line
+			if wordLength > l:	break
+
+			# compute cost for remaining suffix array
+			suffixCost = foo(j + 1)
+			currCost = computeCost(l, wordLength)
+
+			# minimise the cost and mainted parent accordingly
+			if currCost + suffixCost < cost:
+				cost = currCost + suffixCost
+				parent[i] = j
+		return cost
+
+	# calling main function
+	foo(0)
+
+	def joinWords(i, j):
+		# prettifies the words in from index [i, j)
+		line = words[i: j]
+		if len(line) == 1 or j == len(words):
+			return (' '.join(line).ljust(l))
+		else:
+			n, r = divmod(l - (sum(map(len, line)) +
+							   len(line)) + 1, len(line) - 1)
+			narrow = ' ' * (n + 1)
+			if r == 0:
+				return narrow.join(line)
+			else:
+				wide = ' ' * (n + 2)
+				return wide.join(line[:r] + [narrow.join(line[r:])])
+
+	i = 0
+	while i < len(words):
+		t = parent[i]+1
+		yield joinWords(i, t)
+		i = t
+
 if __name__ == "__main__":
 	# file = "test.txt"
 	# def getText(file):
@@ -56,5 +120,8 @@ if __name__ == "__main__":
 	# text = """I am the one, the one who don't need no gun to get respect upon the street"""
 
 	text="The boy walked down the street in a carefree way, playing without notice of what was about him. He didn't hear the sound of the car as his ball careened into the road. He took a step toward it, and in doing so sealed his fate.Sometimes that's just the way it has to be. Sure, there were probably other options, but he didn't let them enter his mind. It was done and that was that. It was just the way it had to be."
-	for t in textJustification(text, lineWidth=40):
+	for t in textJustification2(text, 40):
+		print(t)
+	print(' ')
+	for t in textJustification(text, 40):
 		print(t)
